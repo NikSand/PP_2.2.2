69373cc85c8c9366458b97e14cf008440452110e
Никита <71456672+NikSand@users.noreply.github.com>
2023-10-23 17:14:03 +0300

    Add files via upload

---
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -0,0 +1,35 @@
+package web.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+import web.service.CarServiceImp;
+
+
+
+@Controller
+public class CarController {
+
+    @Autowired
+    private CarService carServiceImp = new CarServiceImp();
+
+
+
+    @GetMapping(value = "/cars")
+    public String printWelcome(Model model, @RequestParam (defaultValue = "5") int count) {
+
+
+        model.addAttribute("cars", carServiceImp.showCars(count));
+
+        return "cars";
+    }
+
+}
+
