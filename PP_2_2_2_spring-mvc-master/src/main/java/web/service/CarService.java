c6616ca91c4a3762a81be592449126150565c4b8
Никита <71456672+NikSand@users.noreply.github.com>
2023-10-23 16:24:07 +0300

    Add files via upload

---
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarService.java
@@ -0,0 +1,12 @@
+package web.service;
+
+import web.model.Car;
+import java.util.List;
+
+
+public interface CarService {
+    static List<Car> showCars( int number) {
+        return null;
+    }
+
+}
